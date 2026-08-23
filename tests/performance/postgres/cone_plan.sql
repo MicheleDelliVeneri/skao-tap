a84991518c3a5455b4748b866a1c713208c9b8b6
Michele Delli Veneri <46787099+MicheleDelliVeneri@users.noreply.github.com>
2026-08-23 10:53:20 +0100

    test: match cone workload to translated ADQL expression

--- tests/performance/postgres/cone_plan.sql
+++ tests/performance/postgres/cone_plan.sql
@@ -1,7 +1,7 @@
 EXPLAIN (ANALYZE, BUFFERS, SETTINGS, FORMAT JSON)
 SELECT count(*)
 FROM perf.catalog
-WHERE position <@ scircle(
+WHERE spoint(radians(ra), radians(dec)) <@ scircle(
     spoint(radians(62.3), radians(-65.5)),
     radians(0.5)
 );
